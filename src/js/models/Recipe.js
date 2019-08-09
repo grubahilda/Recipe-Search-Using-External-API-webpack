3a25a370127d6aba9ca97111325c24987f35236c
grubahilda <marta.pozaroszczyk@gmail.com>
2019-08-09 16:54:19 +0200

    Remove https due to mixed content warinig

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,122 +1,131 @@
-import axios from 'axios';
-import {
-    key,
-    proxy
-} from '../config';
-
+import axios from "axios";
+import { key, proxy } from "../config";
 
 export default class Recipe {
-    constructor(id) {
-        this.id = id; // each recipe has an id
+  constructor(id) {
+    this.id = id; // each recipe has an id
+  }
+
+  async getRecipe() {
+    try {
+      const res = await axios(
+        `${proxy}http://www.food2fork.com/api/get?key=${key}&rId=${this.id}`
+      );
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.publisher;
+      this.image = res.data.recipe.image_url;
+      this.url = res.data.recipe.source_url;
+      this.ingredients = res.data.recipe.ingredients;
+    } catch (error) {
+      console.log(error);
+      alert("Something went wrong.");
     }
+  }
 
-    async getRecipe() {
+  calcTime() {
+    // Assuming that we need 10 minutes per 3 ingredients
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 10;
+  }
 
-        try {
-            const res = await axios(`${proxy}https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`);
-            this.title = res.data.recipe.title;
-            this.author = res.data.recipe.publisher;
-            this.image = res.data.recipe.image_url;
-            this.url = res.data.recipe.source_url;
-            this.ingredients = res.data.recipe.ingredients;
+  calcServings() {
+    this.servings = 4;
+  }
 
-        } catch (error) {
-            console.log(error);
-            alert('Something went wrong.');
+  parseIngredients() {
+    const unitsLong = [
+      "tablespoons",
+      "tablespoon",
+      "ounces",
+      "ounce",
+      "teaspoons",
+      "teaspoon",
+      "cups",
+      "pounds"
+    ];
+    const unitsShort = [
+      "tbsp",
+      "tbsp",
+      "oz",
+      "oz",
+      "tsp",
+      "tsp",
+      "cup",
+      "pound"
+    ];
+    const units = [...unitsShort, "kg", "g"];
+
+    const newIngredients = this.ingredients.map(el => {
+      // Uniform the units
+      let ingredient = el.toLowerCase();
+
+      unitsLong.forEach((unit, i) => {
+        ingredient = ingredient.replace(unit, unitsShort[i]);
+      });
+
+      // Remove paranthesis
+      ingredient = ingredient.replace(/ *\([^)]*\) */g, " ").trim();
+
+      // Parse ingredients into count, unit and ingredient
+
+      const splitIngredient = ingredient.split(" ");
+      const unitIndex = splitIngredient.findIndex(el2 => units.includes(el2));
+
+      let ingredientObject;
+
+      if (unitIndex === 0) {
+        // There is no number but there is a unit
+        ingredientObject = {
+          amount: 1,
+          unit: splitIngredient[unitIndex],
+          ingredient
+        };
+      } else if (unitIndex > -1) {
+        // Number and unit present
+        const arrCount = splitIngredient.slice(0, unitIndex);
+
+        let amount;
+        if (arrCount.length === 1) {
+          amount = eval(splitIngredient[0].replace("-", "+"));
+        } else {
+          amount = eval(splitIngredient.slice(0, unitIndex).join("+"));
         }
-    };
 
-    calcTime() {
-        // Assuming that we need 10 minutes per 3 ingredients
-        const numIng = this.ingredients.length;
-        const periods = Math.ceil(numIng / 3);
-        this.time = periods * 10;
-    };
+        ingredientObject = {
+          amount,
+          unit: splitIngredient[unitIndex],
+          ingredient: splitIngredient.slice(unitIndex + 1).join(" ")
+        };
+      } else if (parseInt(splitIngredient[0], 10)) {
+        // Only number present, no unit
+        ingredientObject = {
+          amount: parseInt(splitIngredient[0], 10),
+          unit: "",
+          ingredient: splitIngredient.slice(1).join(" ")
+        };
+      } else if (unitIndex === -1) {
+        // No number and no unit
+        ingredientObject = {
+          amount: 1,
+          unit: "",
+          ingredient
+        };
+      }
 
-    calcServings() {
-        this.servings = 4;
-    };
+      return ingredientObject;
+    });
+    this.ingredients = newIngredients;
+  }
 
-    parseIngredients() {
-        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
-        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
-        const units = [...unitsShort, 'kg', 'g']        
+  updateServings(type) {
+    // When servings are decreased
+    const newServings = type === "dec" ? this.servings - 1 : this.servings + 1;
 
-        const newIngredients = this.ingredients.map(el => {
-            // Uniform the units
-            let ingredient = el.toLowerCase()
-            
-            unitsLong.forEach((unit, i) => {
-                ingredient = ingredient.replace(unit, unitsShort[i]);
-            });
+    this.ingredients.forEach(ingredient => {
+      ingredient.amount *= newServings / this.servings;
+    });
 
-            // Remove paranthesis
-            ingredient = ingredient.replace(/ *\([^)]*\) */g, " ").trim();            
-
-            // Parse ingredients into count, unit and ingredient
-            
-            const splitIngredient = ingredient.split(" ");
-            const unitIndex = splitIngredient.findIndex(el2 => units.includes(el2));
-
-            let ingredientObject;           
-             
-            
-             if (unitIndex === 0) {
-                // There is no number but there is a unit
-                ingredientObject = {
-                    amount: 1,
-                    unit: splitIngredient[unitIndex],
-                    ingredient
-                };
-            }
-                else if(unitIndex > -1) {
-                // Number and unit present
-                const arrCount = splitIngredient.slice(0, unitIndex);
-                
-                let amount;
-                if(arrCount.length === 1) {
-                    amount = eval(splitIngredient[0].replace("-", "+"));
-                } else {
-                    amount = eval(splitIngredient.slice(0, unitIndex).join('+'));
-                }
-                
-                ingredientObject = {
-                    amount,
-                    unit: splitIngredient[unitIndex],
-                    ingredient: splitIngredient.slice(unitIndex + 1).join(" ")
-                };
-
-            } else if(parseInt(splitIngredient[0], 10)) {
-                // Only number present, no unit
-                ingredientObject = {
-                    amount: parseInt(splitIngredient[0], 10),
-                    unit: '',
-                    ingredient: splitIngredient.slice(1).join(" ")
-                };
-
-            } else if(unitIndex === -1) {
-                // No number and no unit
-                ingredientObject = {
-                    amount: 1,
-                    unit: '',
-                    ingredient
-                };
-            } 
-
-            return ingredientObject;
-        });
-        this.ingredients = newIngredients;
-    }
-
-    updateServings (type) {
-        // When servings are decreased
-        const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
-
-        this.ingredients.forEach(ingredient => {
-            ingredient.amount *= newServings / this.servings;
-        });
-
-        this.servings = newServings;
-    }
-
+    this.servings = newServings;
+  }
 }
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,23 +1,23 @@
-// Importing promis based HTTP client 
-import axios from 'axios';
-import {key, proxy} from '../config';
+// Importing promis based HTTP client
+import axios from "axios";
+import { key, proxy } from "../config";
 
 export default class Search {
+  constructor(query) {
+    this.query = query; // query is a input phrase from the user
+  }
 
-    constructor(query) {
-        this.query = query; // query is a input phrase from the user 
-    }
+  // getting the results from food2fork using axios
+  async getResults() {
+    try {
+      const res = await axios(
+        `${proxy}http://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+    } catch (error) {
+      console.log(error);
 
-    // getting the results from food2fork using axios
-    async getResults() {
-        try {
-            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);            
-            this.result = res.data.recipes;    
-        } catch (error) {
-            console.log(error);
-            
-            alert(error)
-        }
-    
+      alert(error);
     }
+  }
 }
